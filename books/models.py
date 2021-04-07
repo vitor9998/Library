0e41e0d79f6517a31d1511cdd692f1e25e61003c
Vitor Oikawa <vitor9998@gmail.com>
2021-04-07 18:22:49 -0300

    Criando Models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Books(model.Models):
+    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    titile = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    state = models.CharField(max_length=50)
+    pages = models.IntegerField()
+    publishing_company = models.CharField(max_length=255)
+    create_at = models.DateField(auto_now_add=True)
